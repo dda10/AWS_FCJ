d58716dbbd45c5e45d27ab60eeac420a754af516
Duy Anh <anhdd9@techcombank.com.vn>
2025-04-03 15:29:45 +0700

    rollback state

--- lab1/iac/terraform/tgw/provider.tf
+++ lab1/iac/terraform/tgw/provider.tf
@@ -2,17 +2,17 @@ provider "aws" {
   region = var.region
 }
 
-terraform {
-  cloud {
-    organization = "anhdd-private-org"
-    hostname     = "app.terraform.io" # Optional; defaults to app.terraform.io
+# terraform {
+#   cloud {
+#     organization = "anhdd-private-org"
+#     hostname     = "app.terraform.io" # Optional; defaults to app.terraform.io
 
-    workspaces {
-      project = "AWS_FCJ"
+#     workspaces {
+#       project = "AWS_FCJ"
 
-      tags = {
-        source = "cli"
-      }
-    }
-  }
-}
+#       tags = {
+
+#       }
+#     }
+#   }
+# }
