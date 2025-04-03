5f02a93067929fd73178adb7885f8474fd3b5eb1
Duy Anh <anhdd9@techcombank.com.vn>
2025-04-03 15:25:08 +0700

    fix

--- lab1/iac/terraform/vpg/provider.tf
+++ lab1/iac/terraform/vpg/provider.tf
@@ -2,4 +2,17 @@ provider "aws" {
   region = var.region
 }
 
+terraform {
+  cloud {
+    organization = "anhdd-private-org"
+    hostname     = "app.terraform.io" # Optional; defaults to app.terraform.io
 
+    workspaces {
+      project = "AWS_FCJ"
+
+      tags = {
+        source = "cli"
+      }
+    }
+  }
+}
